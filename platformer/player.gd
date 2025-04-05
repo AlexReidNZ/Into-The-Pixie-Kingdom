82242301ad5e736d2cbc54ee9d012539c8610db5
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2025-04-05 21:55:30 +1300

    feat(player controller): run

--- platformer/player.gd
+++ platformer/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
 
-@export_range(1, 500) var SPEED = 300.0
-@export_range(1, 500) var JUMP_VELOCITY = 400.0
+@export_range(1, 300) var speed = 100.0
+@export_range(0, 1) var deceleration = 0.5
+@export_range(1, 2) var run_speed_multiplier = 1.69
+@export_range(1, 300) var jump_velocity = 200.0
+
+@onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _process(delta: float) -> void:
@@ -13,14 +17,16 @@ func _process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		# Negative Y direction is upwards in 2D space
-		velocity.y = -JUMP_VELOCITY
+		velocity.y = -jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+	var speed_mutliplier = 1 + Input.get_action_strength("run") * run_speed_multiplier
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed * speed_mutliplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Deceleration by speed
+		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
 
 	move_and_slide()
