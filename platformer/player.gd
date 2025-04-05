b17071fc838e2b7c2d46cb340ac198596e46fd1f
otherotherjosh <itsotherjosh@gmail.com>
2025-04-06 08:11:30 +1200

    feat(player): add coyote time

--- platformer/player.gd
+++ platformer/player.gd
@@ -6,6 +6,14 @@ extends CharacterBody2D
 @export_range(1, 2) var run_speed_multiplier = 1.69
 @export_range(1, 300) var jump_velocity = 200.0
 
+## Time (in milliseconds) the player can press jump before/after touching the ground.
+## Named after Wile E. Coyote from Looney Tunes, may he rest in peace. <3
+@export_range(0, 250, 5) var coyote_time_ms := 100
+
+var last_jump_input_time_ms := -1
+var last_on_floor_time_ms := -1
+var jump_queued := false
+
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 
 
@@ -13,11 +21,11 @@ func _process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		last_on_floor_time_ms = Time.get_ticks_msec()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		# Negative Y direction is upwards in 2D space
-		velocity.y = -jump_velocity
+	if jump_queued:
+		try_jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -30,3 +38,23 @@ func _process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
 
 	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		queue_jump()
+		
+		
+func queue_jump() -> void:
+	last_jump_input_time_ms = Time.get_ticks_msec()
+	jump_queued = true
+	
+
+func try_jump() -> void:
+	if (Time.get_ticks_msec() < last_on_floor_time_ms + coyote_time_ms 
+			and Time.get_ticks_msec() < last_jump_input_time_ms + coyote_time_ms):
+		jump_queued = false
+		last_on_floor_time_ms = -1
+		# Negative Y direction is upwards in 2D space
+		velocity.y = -jump_velocity
+		
