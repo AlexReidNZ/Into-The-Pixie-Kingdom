46303dd399b946f68c917b6ecbcc3f39f4f46d2f
Alex Reid <alexanderreidnz@gmail.com>
2025-04-13 12:42:48 +1200

    feat: add logic for checking for colliding tiles to puzzle piece

--- Scripts/puzzle/PuzzleBlock.gd
+++ Scripts/puzzle/PuzzleBlock.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
 @onready var puzzleManager = $".."
+@onready var collider = $Area2D
 var draggable = false
 var is_droppable = false
 var body_Ref
 
 func  _process(delta: float) -> void:
 	if draggable:
+		if Input.is_action_just_pressed("click"): #Set slot taken to false for all colliding shapes
+			for slot in collider.get_overlapping_areas():
+				slot.slotTaken = false
+				print(str(slot.name," ",slot.slotTaken))
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position()
 		elif Input.is_action_just_released("click"):
 			DropPiece()
-
 
 func _on_area_2d_mouse_entered() -> void:
 	if not puzzleManager.is_dragging:
@@ -26,4 +30,7 @@ func _on_area_2d_mouse_exited() -> void:
 
 func DropPiece():
 	print("drag stopped")
+	for slot in collider.get_overlapping_areas():
+		slot.slotTaken = true
+		print(str(slot.name," ",slot.slotTaken))
 	puzzleManager.is_dragging = false
