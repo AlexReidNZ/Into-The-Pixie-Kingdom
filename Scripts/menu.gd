f95178aed821d1f0771b05d4275f0daa24b453e2
otherotherjosh <itsotherjosh@gmail.com>
2025-04-29 13:11:12 +1200

    feat: change scale settings

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
+
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://platformer/platform_level.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
