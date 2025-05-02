0819fa5bab1f85e0cd5f5d95cfcb8141a3042883
Amber <amberj3lly@gmail.com>
2025-05-02 13:18:17 +1200

    fix: Dialog was broken after merge and it has been fixed up

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
