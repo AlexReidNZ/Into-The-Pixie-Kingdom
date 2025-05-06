bbcd31966e0cc98b7af960e821927e153a2b027a
AlexReidNZ <reidaj7@student.op.ac.nz>
2025-05-06 14:18:21 +1200

    feat: add puzzle piece rotation

--- Scripts/puzzle/puzzleManager.gd
+++ Scripts/puzzle/puzzleManager.gd
@@ -6,6 +6,8 @@ var current_dragging_piece = null
 @onready var gridSlots = get_child(0).get_children()
 
 func _process(delta: float) -> void:
+	if is_dragging and Input.is_action_just_pressed("rotate puzzle piece"):
+		rotate_piece()
 	if check_puzzle_won() and !puzzle_won:
 		win_puzzle()
 
@@ -18,3 +20,7 @@ func check_puzzle_won():
 		if !slot.slotTaken:
 			return false
 	return true
+
+func rotate_piece():
+	if current_dragging_piece:
+		current_dragging_piece.rotate(deg_to_rad(90))
