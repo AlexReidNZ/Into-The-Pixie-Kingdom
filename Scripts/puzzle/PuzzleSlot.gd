e1345902e758cef5297d9f6c098642d0637ef2de
Alex Reid <alexanderreidnz@gmail.com>
2025-05-15 08:45:47 +1200

    feat: add implementation for second puzzle to level

--- Scripts/puzzle/PuzzleSlot.gd
+++ Scripts/puzzle/PuzzleSlot.gd
@@ -1,2 +1,5 @@
 extends Area2D
 var slotTaken = false
+
+func _ready():
+	add_to_group("Colliders")
--- Scripts/puzzle/puzzle_indicator.gd
+++ Scripts/puzzle/puzzle_indicator.gd
@@ -1,16 +1,23 @@
 extends Area2D
 
 @export var puzzle_scene : PackedScene
+@export var puzzle_group : String
+@export var fixed_sprite : Texture2D
+@export var sprite_object : Sprite2D
 @onready var canvas = $CanvasLayer
 var clickable = false
 var puzzle_active = false
 var puzzle_instance
+var puzzle_slots
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	puzzle_instance = puzzle_scene.instantiate()
 	canvas.add_child(puzzle_instance)
 	puzzle_instance.hide()
+	puzzle_slots = get_tree().get_first_node_in_group(puzzle_group).get_children()
+	for i in puzzle_slots:
+		i.get_child(0).disabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,11 +36,15 @@ func toggle_puzzle():
 	if puzzle_instance: #make sure puzzle instantiated correctly
 		if puzzle_active:
 			puzzle_instance.show()
+			for i in puzzle_slots:
+				i.get_child(0).disabled = false
 		else:
 			puzzle_instance.hide()
+			for i in puzzle_slots:
+				i.get_child(0).disabled = true
 			
 func _on_puzzle_won():
 	print("puzzle has been won")
 	toggle_puzzle()
-	#change sprite of puzzle object to fixed sprite
+	sprite_object.texture = fixed_sprite
 	self.hide()
