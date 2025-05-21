b1991431a5c59e33c8fc74e95c933c4abbf8eb12
Alex Reid <alexanderreidnz@gmail.com>
2025-05-21 15:36:02 +1200

    feat: add toggle puzzle on e input

--- Scripts/puzzle/detection_area.gd
+++ Scripts/puzzle/detection_area.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
 @onready var indicator = $".."
+var player_in_area = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name != "Player":
 		return
 	indicator.visible = true
+	player_in_area = true
 	
 func _on_body_exited(body: Node2D) -> void:
 	if body.name != "Player":
 		return
 	indicator.visible = false
+	player_in_area = false
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("open_puzzle") && player_in_area:
+		indicator.toggle_puzzle()
